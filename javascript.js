f1298a0705b300c88edd43448d0309c538c542a2
ki-ki13 <rizkimahjati845@students.unnes.ac.id>
2020-12-21 00:51:25 +0700

    admin-changes

--- javascript.js
+++ javascript.js
@@ -2,3 +2,18 @@
 function JSalert(){
 	swal("Congrats!", ", Added to cart!", "success");
 }
+
+var coll = document.getElementsByClassName("box");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
